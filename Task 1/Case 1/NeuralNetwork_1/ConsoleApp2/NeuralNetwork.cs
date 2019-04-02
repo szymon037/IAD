d2a1ff5ba524d86702182f30b52c508cfec7b69d
shy <szymon.lost@gmail.com>
2019-04-02 15:52:04 +0200

    Edited Matrix Library and added 1st case of 1st task

---
+++ Task 1/Case 1/NeuralNetwork_1/ConsoleApp2/NeuralNetwork.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork_1
+{
+    class NeuralNetwork
+    {
+        public Matrix.Matrix hiddenWeights;
+        public Matrix.Matrix outputWeights;
+        private Matrix.Matrix biasHidden;
+        private Matrix.Matrix biasOutput;
+        private bool useBias;
+        private double learningRate;
+
+        public NeuralNetwork(int inputAmount, int hiddenAmount, int outputAmount, bool bias)
+        {
+            learningRate = 0.1;
+
+            useBias = bias;          
+
+            hiddenWeights = new Matrix.Matrix(hiddenAmount, inputAmount);    // 2 x 3
+            outputWeights = new Matrix.Matrix(outputAmount, hiddenAmount);   // 1 x 2
+            hiddenWeights.RandomizeMatrix(-5, 5);
+            outputWeights.RandomizeMatrix(-5, 5);
+
+
+            biasHidden = new Matrix.Matrix(hiddenAmount, 1);
+            biasOutput = new Matrix.Matrix(outputAmount, 1);
+            biasHidden.RandomizeMatrix(-5, 5);
+            biasOutput.RandomizeMatrix(-5, 5);
+
+            
+        }
+
+        public Matrix.Matrix feedForward(int[] inputArray)
+        {
+            Matrix.Matrix inputMatrix = new Matrix.Matrix(inputArray);    // 3 x 1
+            Matrix.Matrix hiddenOutput = hiddenWeights * inputMatrix;
+            if (useBias)
+            {
+                hiddenOutput += biasHidden;
+            }
+            activationFunction(hiddenOutput);
+
+            Matrix.Matrix outputsOutput = outputWeights * hiddenOutput;
+            if (useBias)
+            {
+                outputsOutput += biasOutput;
+            }
+            activationFunction(outputsOutput);
+            return outputsOutput;
+        }
+
+        public void train(int[] inputArray, int[] targetArray)
+        {
+            Matrix.Matrix inputMatrix = new Matrix.Matrix(inputArray);    // 3 x 1
+            Matrix.Matrix hiddenOutput = hiddenWeights * inputMatrix;
+            if (useBias)
+            {
+                hiddenOutput += biasHidden;
+            }
+            activationFunction(hiddenOutput);
+
+            Matrix.Matrix outputsOutput = outputWeights * hiddenOutput;
+            if (useBias)
+            {
+                outputsOutput += biasOutput;
+            }
+            activationFunction(outputsOutput);
+
+            Matrix.Matrix targetMatrix = new Matrix.Matrix(targetArray);
+            Matrix.Matrix outputErrorsMatrix = targetMatrix - outputsOutput;
+
+            mapMatrix(outputsOutput);
+
+            Matrix.Matrix gradients_output = outputErrorsMatrix * learningRate;
+            gradients_output.HadamardProduct(outputsOutput);
+            Matrix.Matrix hiddenTransposed = hiddenOutput.TransposeMatrix();
+            Matrix.Matrix outputs_deltas = gradients_output * hiddenTransposed;
+
+            outputWeights += outputs_deltas;
+            biasOutput += gradients_output;
+
+            //hidden layer errors
+            Matrix.Matrix outputWeights_transposed = outputWeights.TransposeMatrix();
+            Matrix.Matrix hiddenErrorsMatrix = outputWeights_transposed * outputErrorsMatrix;
+
+            mapMatrix(hiddenOutput);
+
+            Matrix.Matrix gradients_hidden = hiddenErrorsMatrix * learningRate;
+            gradients_hidden.HadamardProduct(hiddenOutput);
+
+            Matrix.Matrix hidden_deltas = gradients_hidden * inputMatrix.TransposeMatrix();
+            hiddenWeights += hidden_deltas;
+            biasHidden += gradients_hidden;
+
+        }
+
+        public void activationFunction(Matrix.Matrix m)
+        {
+            for (int i = 0; i < m.row; ++i)
+            {
+                for (int j = 0; j < m.column; ++j)
+                {
+                    m.tab[i, j] = sigmoid(m.tab[i, j]);
+                }
+            }
+        }
+
+        public void mapMatrix(Matrix.Matrix m)
+        {
+            for (int i = 0; i < m.row; ++i)
+            {
+                for (int j = 0; j < m.column; ++j)
+                {
+                    m.tab[i, j] = dSigmoid(m.tab[i, j]);
+                }
+            }
+        }
+
+        private double sigmoid(double x)
+        {
+            return (1 / (1 + Math.Exp(-x)));
+        }
+
+        private double dSigmoid(double x)
+        {
+            //double sigX = sigmoid(x);
+            //return sigX * (1 - sigX);
+            return x * (1 - x);
+        }
+    }
+
+
+
+}
---
+++ Task 1/Case 1/NeuralNetwork_1/ConsoleApp2/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Matrix.Matrix a = new Matrix.Matrix(5, 5);
+            Matrix.Matrix b = new Matrix.Matrix(5, 5);
+
+            a.RandomizeMatrix(-5, 5);
+            a.DisplayMatrix();
+            b = a.TransposeMatrix();
+            Console.Write('\n');
+            b.DisplayMatrix();*/
+            /*NeuralNetwork nn = new NeuralNetwork(3, 2, 1, true);
+            int[] a = new int[3];
+            for (int i = 0; i < 3; ++i)
+                a[i] = i;
+
+            Matrix.Matrix output = new Matrix.Matrix(1, 1);
+
+            output = nn.feedForward(a);
+            output.DisplayMatrix();*/
+            /*Matrix.Matrix a = new Matrix.Matrix(3, 3);
+            a.RandomizeMatrix(-3, 3);
+            a.DisplayMatrix();
+            Console.Write('\n');
+            nn.activationFunction(a);
+            a.DisplayMatrix();*/
+
+
+            /*Matrix.Matrix a = new Matrix.Matrix(2, 1);
+            Matrix.Matrix b = new Matrix.Matrix(1, 2);
+            a.RandomizeMatrix(0, 5);
+            b.RandomizeMatrix(0, 5);
+
+            a.DisplayMatrix();
+            b.DisplayMatrix();
+
+
+            Matrix.Matrix c = a * b;
+            c.DisplayMatrix();*/
+
+
+            /*int[] a = new int[3];
+            for (int i = 0; i < 3; ++i)
+                a[i] = i;
+            Console.Write(a.Length);
+            Matrix.Matrix m = new Matrix.Matrix(a, true);
+            m.DisplayMatrix();*/
+
+
+            // XOR
+
+            /*NeuralNetwork nn = new NeuralNetwork(2, 4, 1, true);
+
+            int[] input = new int[2];
+            int[] target = new int[1];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10000; ++i)
+            {
+                input[0] = rnd.Next(0, 2);
+                input[1] = rnd.Next(0, 2);
+
+                target[0] = input[0] ^ input[1];
+
+                //Console.WriteLine("\ninput0: " + input[0] + " input1: " + input[1] + " target: " + target[0]);
+                nn.train(input, target);
+            }
+
+            input[0] = 0;
+            input[1] = 0;
+            target[0] = 0;
+
+            Matrix.Matrix output = nn.feedForward(input);
+            output.DisplayMatrix();
+            Console.Write('\n');
+
+            input[0] = 1;
+            input[1] = 1;
+            target[0] = 0;
+
+            output = nn.feedForward(input);
+            output.DisplayMatrix();
+            Console.Write('\n');
+
+            input[0] = 0;
+            input[1] = 1;
+            target[0] = 1;
+
+            output = nn.feedForward(input);
+            output.DisplayMatrix();
+            Console.Write('\n');
+
+            input[0] = 1;
+            input[1] = 0;
+            target[0] = 1;
+
+            output = nn.feedForward(input);
+            output.DisplayMatrix();
+            Console.Write('\n');
+            */
+
+
+            //ZADANIE 1
+
+            int[][] data = new int[4][];
+            data[0] = new int[] { 1, 0, 0, 0 };
+            data[1] = new int[] { 0, 1, 0, 0 };
+            data[2] = new int[] { 0, 0, 1, 0 };
+            data[3] = new int[] { 0, 0, 0, 1 };
+
+            NeuralNetwork nn = new NeuralNetwork(4, 3, 4, true);
+            int n = 0;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10000; ++i)
+            {
+                n = rnd.Next(0, 4);
+                nn.train(data[n], data[n]);
+            }
+
+            Matrix.Matrix output = new Matrix.Matrix(4, 1);
+
+            for (int i = 0; i < 4; ++i)
+            {
+                output = nn.feedForward(data[i]);
+                output.DisplayMatrix();
+                Console.Write('\n');
+            }
+
+        }
+}
+}
