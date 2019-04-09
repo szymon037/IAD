5e6a496d621c93df7b44bddc2b889f79d2e8f990
216895 Suszek Michał <216895@edu.p.lodz.pl>
2019-04-09 15:36:16 +0200

    Task 1 Case 2 Starter
    Basic Implementation
    trian doesn't work properly

---
+++ Task 1/Case 2/Neural_Network_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.WindowsForms;
+using OxyPlot.Series;
+
+namespace Neural_Network_2
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
