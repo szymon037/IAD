2dcc858cafaa3592061c30bccd1c88e4ddc5795a
216895 Suszek Michał <216895@edu.p.lodz.pl>
2019-05-01 13:37:49 +0200

    First commit of task 2
    Added random points generator

---
+++ Task 2/Case 1-2/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace task_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pB = new System.Windows.Forms.PictureBox();
+            this.tBpoints = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pB
+            // 
+            this.pB.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.pB.Location = new System.Drawing.Point(12, 12);
+            this.pB.Name = "pB";
+            this.pB.Size = new System.Drawing.Size(706, 583);
+            this.pB.TabIndex = 0;
+            this.pB.TabStop = false;
+            this.pB.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pB_MouseDown);
+            this.pB.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pB_MouseUp);
+            // 
+            // tBpoints
+            // 
+            this.tBpoints.Location = new System.Drawing.Point(751, 68);
+            this.tBpoints.Name = "tBpoints";
+            this.tBpoints.Size = new System.Drawing.Size(100, 20);
+            this.tBpoints.TabIndex = 1;
+            this.tBpoints.Text = "200";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(748, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ilość punktów";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(794, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 607);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tBpoints);
+            this.Controls.Add(this.pB);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pB;
+        private System.Windows.Forms.TextBox tBpoints;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Task 2/Case 1-2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //init variables 
+        public int initalX;
+        public int initalY;
+        public int finalX;
+        public int finalY;
+        public List<Point> Data = new List<Point>();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pB_MouseDown(object sender, MouseEventArgs e)
+        {
+            initalX = e.X;
+            initalY = e.Y;
+        }
+
+        private void pB_MouseUp(object sender, MouseEventArgs e)
+        {
+            finalX = e.X;
+            finalY = e.Y;
+            if(finalX < initalX)
+            {
+                initalX = initalX + finalX;
+                finalX = initalX - finalX;
+                initalX = initalX - finalX;
+            }
+            if(finalY < initalY)
+            {
+                initalY = initalY + finalY;
+                finalY = initalY - finalY;
+                initalY = initalY - finalY;
+            }
+            DrawRectangle(initalX, initalY, finalX, finalY);
+        }
+
+        private void DrawRectangle(int initalX, int initalY, int finalX, int finalY)
+        {
+            System.Drawing.Graphics g;
+            //pB.Refresh();
+            g = pB.CreateGraphics();
+            Pen mypen = new System.Drawing.Pen(Brushes.Black);
+            SolidBrush brush = new SolidBrush(Color.LimeGreen);
+            g.DrawRectangle(mypen, initalX, initalY, Math.Abs(initalX-finalX), Math.Abs(initalY - finalY));
+
+            Random rng = new Random();
+            List<Point> temp = new List<Point>();
+            int pointer = Data.Count();
+            for(int i = 0; i < Convert.ToInt32(tBpoints.Text); i++)
+            {
+                Point p = new Point(rng.Next(initalX, finalX), rng.Next(initalY, finalY));
+                Data.Add(p);
+                Rectangle rect = new Rectangle(Data[pointer + i], new Size(4, 4));
+                g.FillRectangle(brush, rect);
+            }
+        }
+
+        private void InitDraw()
+        {
+            System.Drawing.Graphics g = pB.CreateGraphics(); ;
+            pB.Refresh();
+            SolidBrush brush = new SolidBrush(Color.LimeGreen);
+            for(int i = 0; i < Data.Count(); i++)
+            {
+                Rectangle rect = new Rectangle(Data[i], new Size(4, 4));
+                g.FillRectangle(brush, rect);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitDraw();
+        }
+    }
+}
